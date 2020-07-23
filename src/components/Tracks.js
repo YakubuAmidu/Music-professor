64cd645e69c471e4115678af828e550fd9db2891
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-22 21:23:58 -0700

    Commiting changes

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -3,6 +3,16 @@ import React, { Component } from 'react';
 class Tracks extends Component {
     render() {
         const { tracks } = this.props;
-        
+
+        return (
+            <div>
+                {
+                    tracks.map(track => {
+                        const { track, name } = track;
+                        
+                    })
+                }
+            </div>
+        )
     }
 }
