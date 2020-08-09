b5a8a43001ea4bb6ddd530f49c9af65c116849ae
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-09 00:47:16 -0700

    Commiting changes

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -24535,7 +24535,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65021" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52378" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
